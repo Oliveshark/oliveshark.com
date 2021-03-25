6578b9b9f0649babf1a429d9080d5fec17fbcadc
Linus Probert <linus.probert@gmail.com>
2021-03-25 19:09:45 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import ContactItem from './ContactItem.svelte';
+
+	const contactEntries: any[] = [
+		{
+			link: "https://twitter.com/LiquidityC",
+			label: "Twitter",
+			icon: "fab fa-twitter",
+		},
+		{
+			link: "https://github.com/LiquidityC",
+			label: "GitHub",
+			icon: "fab fa-github",
+		},
+		{
+			link: "https://www.linkedin.com/in/linus-probert/",
+			label: "LinkedIn",
+			icon: "fab fa-linkedin",
+		},
+		{
+			link: "https://store.steampowered.com/curator/33516566-Oliveshark/",
+			label: "Steam",
+			icon: "fab fa-steam",
+		},
+	];
+</script>
+
+<main>
+	<div>
+		<img alt="Oliveshark Logo" src="logo.png">
+	</div>
+	<div>
+		{#each contactEntries as entry}
+			<ContactItem {...entry}/>
+		{/each}
+	</div>
+</main>
+
+<style lang="scss">
+	:global(body) {
+		background-color: #639bff;
+	}
+
+	main {
+		text-align: center;
+		padding: 1em;
+		margin: 0 auto;
+	}
+</style>
---
+++ src/ContactItem.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	export let link: string;
+	export let label: string;
+	export let icon: string;
+</script>
+
+<span class="item">
+	<a href={link}>
+		<i class={icon}></i> {label}
+	</a>
+</span>
+
+<style lang="scss">
+	.item {
+		display: inline-block;
+		margin-left: 1rem;
+	}
+	a {
+		text-decoration: none;
+		color: black;
+
+		&:visited {
+			text-decoration: none;
+			color: black;
+		}
+	}
+</style>
